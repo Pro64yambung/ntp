bce3b8b6c92e03b4f1c1bc81dbe1c5166cc0e2f2
pro64yambung <rohrazer64@gmail.com>
2023-12-10 13:37:28 +0530

    template

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,6 @@
 class ntp::config inherits ntp {
 file { "/etc/ntp.conf" : 
-                        source => "puppet:///modules/ntp/ntp.conf"
+                       # source => "puppet:///modules/ntp/ntp.conf"
+			content => template("ntp/ntp.conf"),
 }
 }
