ee58ac35c89075d03fa090c6b68ca7355f1de115
pro64yambung <rohrazer64@gmail.com>
2023-12-10 13:55:45 +0530

    local variable added

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,5 @@
 class ntp::config inherits ntp {
+$os_name = $facts['operatingsystem']
 file { "/etc/ntp.conf" : 
                        # source => "puppet:///modules/ntp/ntp.conf"
 			content => template("ntp/ntp.conf.erb"),
